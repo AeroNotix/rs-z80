42946ed0ca59fd21cbc33e953f827929683c224f
Aaron France <aaron.l.france@gmail.com>
2021-05-19 16:24:12 +0200

    fix main's use of Opcode constructor

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ mod instructions;
 
 fn main() {
     for raw_opcode in [0b1111000, 0b1000001, 0b1001010, 0b1010011] {
-        let opcode = instructions::Opcode::new(raw_opcode).decode();
+        let opcode = instructions::Opcode::from_u8(raw_opcode).decode();
         if let instructions::Instruction::Unknown =  opcode {
-            println!("{:?}", instructions::Opcode::new(raw_opcode));
+            println!("{:?}", instructions::Opcode::from_u8(raw_opcode));
         }
         println!("{:?}", opcode);
     }
