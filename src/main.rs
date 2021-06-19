e50968b03f890110c33bc8fa6dca9504fc1f66e5
Aaron France <aaron.l.france@gmail.com>
2021-06-19 16:06:04 +0200

    slightly improve main

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::fs;
 mod instructions;
 
-fn main() {
-    let mut cpu = instructions::CPU::new(fs::read("src/roms/ld.rom").expect("Unable to read file"));
+fn run_prog(path: &str) {
+    let mut cpu = instructions::CPU::new(fs::read(path).expect("Unable to read file"));
     for _ in 1..cpu.program.len() {
         cpu.decode(cpu.fetch()).execute(&mut cpu);
     }
+}
+
+fn main() {
+    run_prog("src/roms/ld.rom");
+    run_prog("src/roms/inc_dec.rom");
 }
